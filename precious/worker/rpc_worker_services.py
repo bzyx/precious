56a3abd733e810dc0693aec0f20c62b7365b64ed
Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
2014-02-03 00:02:28 +0100

    New logger formater, newer api for rpc call's

--- precious/worker/rpc_worker_services.py
+++ precious/worker/rpc_worker_services.py
@@ -13,11 +13,10 @@ from precious.utils import get_config_path
 class PreciousWorkerService(rpyc.Service):
 
     def on_connect(self):
-        print "on_connect(self):"
-        logger.info("TEST MESSAGE")
+        logger.info("CONNECTED")
 
     def on_disconnect(self):
-        print "on_disconnect(self):"
+        logger.info("DISCONNECTED")
 
     def exposed_sysinfo(self):
         print "Sending sysinfo"
@@ -27,11 +26,19 @@ class PreciousWorkerService(rpyc.Service):
 
     def exposed_run_command(self, command):
         print "RUNNING: ", command
-        p = run(command, stdout=Capture())
-        return p.stdout.text.split("\n")
+        return run_command(command)
+
+    def exposed_run_command_raw(self, command):
+        print "RUNNING: ", command
+        return run_command_raw_output(command)
+
+    def exposed_run_command_silient(self, command):
+        print "RUNNING: ", command
+        return run_command_forget_output(command)
 
 
     def exposed_say_hello_par(self, name):
         print "Hello! " + name
         return "Hello " + name
+
 
