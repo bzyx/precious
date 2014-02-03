060aa8fa7914986106d4d1a22fc6e17329bb3a41
Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
2014-02-04 00:24:51 +0100

    Sheduler update

--- precious/sheduler/tasks.py
+++ precious/sheduler/tasks.py
@@ -9,3 +9,12 @@ from precious import db, models
 def check_remote_git_revision():
     logger.info("Checking project remote revision")
     print models.Project.query.all()
+
+
+def check_should_buid():
+    logger.info("Checking for project need to be build")
+    # for proj in models.Project.query.all():
+    #   if proj.schedule - timedelta?
+    #         proj.schedule =
+    #        proj.build())
+
--- runsheduler.py
+++ runsheduler.py
@@ -1,33 +1,24 @@
-"""
-Basic example showing how to start the scheduler and schedule a job that executes on 3 second intervals.
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import logging
-from datetime import datetime
 
 from apscheduler.scheduler import Scheduler
-from apscheduler.jobstores.sqlalchemy_store import SQLAlchemyJobStore
-
-from precious.utils import get_db_uri
+#from apscheduler.jobstores.sqlalchemy_store import SQLAlchemyJobStore
+#from precious.utils import get_db_uri
 from precious.sheduler.tasks import *
 
 
-
 logger = logging.getLogger("scheduler")
-
-def tick():
-    print('Tick! The time is: %s' % datetime.now())
-    logger.info('Tick! The time is: %s' % datetime.now())
 
 
 if __name__ == '__main__':
     scheduler = Scheduler(standalone=True, misfire_grace_time=10,
                           coalescing=False)
-    #Works strange with sqlite...
-    #scheduler.add_jobstore(SQLAlchemyJobStore(get_db_uri()), 'default')
+    #scheduler.add_jobstore(SQLAlchemyJobStore(get_db_uri()), 'file')
 
-    scheduler.add_interval_job(tick, seconds=5)
-    scheduler.add_interval_job(tick, seconds=5)
+    scheduler.add_interval_job(check_remote_git_revision, minutes=10)
+    scheduler.add_interval_job(check_should_buid, minutes=1)
 
     print('Press Ctrl+C to exit')
     try:
