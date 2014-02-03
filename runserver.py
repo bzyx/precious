8c6ebd93ec21675bdf535337f9b346bdb36b22cf
Marius Rejdak <mariuswol@gmail.com>
2014-02-03 01:52:23 +0100

    Config directory fixes

--- runserver.py
+++ runserver.py
@@ -3,5 +3,4 @@
 
 if __name__ == "__main__":
     from precious import app, config
-    print config.getint('webserver', 'port')
     app.run(host=config.get('webserver', 'bind'), port=config.getint('webserver', 'port'))
