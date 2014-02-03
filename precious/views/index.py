45093344264e980223c065a891c2a34980aef1ac
Marius Rejdak <mariuswol@gmail.com>
2014-02-03 18:00:17 +0100

    I hate .pyc files

--- precious/views/index.py
+++ precious/views/index.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template
+from flask.ext.login import login_required
 from precious import *
 from precious.models import *
 
 
 @app.route('/')
 def index():
-    return render_template('base.html', projects=Project.query.all())
+    return render_template('base.html')
+
+
+@app.route('/projects', methods=["GET", "POST"])
+@login_required
+def projects():
+    return render_template('projects.html', projects=Project.query.order_by('projects.name').all())
